58c0bddc95284b277cec571a2e98ad7cac644980
Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>
2025-10-22 16:00:04 +0200

    feat: inherited code
    Signed-off-by: Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>

---
+++ test/token-service/assertions.js
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Apache-2.0
+import { expect } from "chai";
+
+const expectValidHash = (hash, len = 0) => {
+  let regex;
+  if (len && len > 0) {
+    regex = new RegExp(`^0x[a-fA-F0-9]{${len}}$`);
+  } else {
+    regex = new RegExp(`^0x[a-fA-F0-9]*$`);
+  }
+
+  expect(!!hash.match(regex)).to.eq(true);
+};
+
+export {
+  expectValidHash,
+};
